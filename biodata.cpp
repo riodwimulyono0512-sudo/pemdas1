4efc5726aa9a51ee768dfd7548392972ce238e5d
riodwimulyono0512-sudo <riodwimulyono0512@gmail.com>
2026-02-24 16:21:43 +0700

    comit2

---
+++ biodata.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    bool cek = true;
+    char jk;
+    int angka = 10;
+    float tinggi = 180.5;
+    double harga = 200000.00;
+    string nama = "Riokkk";
+
+    jk = 'L';
+    cout << "Selamat Datang" << endl;
+    cout << "di TI UMY\n";
+    cout << "Yogyakarta\n";
+
+    nama = "riokkganteng";
+    cout << "Nama Saya : " << nama;
+
+}
