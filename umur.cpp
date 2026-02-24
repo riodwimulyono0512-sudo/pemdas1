a631e734ec1cf7d8dd33d85faea8eb12667d2dee
riodwimulyono0512-sudo <riodwimulyono0512@gmail.com>
2026-02-24 16:31:30 +0700

    commit4

---
+++ umur.cpp
@@ -0,0 +1,19 @@
+//deklarasi library
+#include<iostream>
+using namespace std;
+
+//program utama
+int main()
+{
+    //deklarasi variabel umur 
+    int umur;
+    //memberi nilai dari variabel
+    umur = 19;
+
+    //menampilkan isi string
+    cout << "Riokk" << endl ;
+    //menampilkan isi integer
+    cout << 78 <<endl;
+    //menampilkan isi dari variable
+    cout << umur <<endl;
+}
