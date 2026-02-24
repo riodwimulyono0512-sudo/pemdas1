b78b81d537772fe034237156eacb9de2218a6505
riodwimulyono0512-sudo <riodwimulyono0512@gmail.com>
2026-02-24 16:26:10 +0700

    commit3

---
+++ persegi.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+
+{//start
+   float p,l,luas;
+
+   //langkah 2 : Masukkan Panjang
+   cout << "Masukkan Panjang : " ;
+   cin >> p;
+   //langkah 3 : Masukkan Lebar
+   cout << "Masukkan Lebar : " ;
+   cin >> l;
+   //langkah 4 : Hitung Luas = p  x l
+   luas = p*l;
+   //langkah 5 :
+   cout << "hasilnya = " << luas ;
+}//selesai
